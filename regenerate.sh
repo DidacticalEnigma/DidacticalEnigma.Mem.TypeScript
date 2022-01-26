8df6d49a3a84238090c569723ba7d7f7bf079603
milleniumbug <milleniumbug@protonmail.com>
2022-01-26 21:16:11 +0100

    Try using an alternative generator

--- regenerate.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-npx autorest --typescript --input-file=swagger.json --package-name=didactical-enigma-mem --add-credentials=false --output-folder=.
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./models";
-export { DidacticalEnigmaMem } from "./didacticalEnigmaMem";
-export { DidacticalEnigmaMemContext } from "./didacticalEnigmaMemContext";
